b251ce6e4b0d1dd42de184125e91c5d92b599bba
Anand-DevOps <135973445+anand1590@users.noreply.github.com>
2025-11-06 10:57:48 +0530

    Refactor to use Azure Functions for HTTP requests

--- randomdrinks.py
+++ randomdrinks.py
@@ -1,16 +1,17 @@
 import json
 import random
+import azure.functions as func
 
 def random_drink():
     drinks = ["coffee", "tea", "beer", "wine", "water", "juice"]
     return random.choice(drinks)
 
-def lambda_handler(event, context):
+def main(req: func.HttpRequest) -> func.HttpResponse:
     drink = random_drink()
     message = f"You should drink some {drink}"
 
-    return{
-        'statusCode': 200,
-        'body': json.dumps({"message": message, "drink": drink})
-    
-    }
+    return func.HttpResponse(
+        json.dumps({"message": message, "drink": drink}),
+        mimetype="application/json",
+        status_code=200
+    )
